edb5d9e4b705fe93bbf307e58bd315ce6ff8c5a1
zhongyu_yu <zhongyu_yu@kingdee.com>
2018-08-01 14:52:11 +0800

    test thread interrupt and resume interrupt status

---
+++ src/main/java/com/zhyyu/learn/concurrency/interrupt/InterruptTest.java
@@ -0,0 +1,42 @@
+package com.zhyyu.learn.concurrency.interrupt;
+
+/**
+ * test thread interrupt and resume interrupt status 
+ * @author Administrator
+ *
+ */
+public class InterruptTest {
+
+	public static void main(String[] args) throws InterruptedException {
+		Thread thread = new Thread(new InterruptedTask());
+		thread.start();
+		
+		thread.interrupt();
+		
+		Thread.sleep(1000);
+		
+		System.out.println(thread.isInterrupted());
+	}
+	
+	static class InterruptedTask implements Runnable {
+
+		@Override
+		public void run() {
+			try {
+				Thread.sleep(10_000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+				
+				/**
+				 * catch InterruptedException 会清清楚中断状态, 重新interrupt 恢复中断状态, 使其他线程可知道该线程被中断过
+				 */
+				Thread.currentThread().interrupt();
+			}
+			
+			// 线程必须是存活的, thread.isInterrupted() 才会返回是否中断, 否则一律返回false
+			while (true);
+		}
+		
+	}
+	
+}
