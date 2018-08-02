9ca084634cfc8231077c90523f9f75f0caaac60a
= <zhyyu2016@gmail.com>
2018-08-02 23:13:40 +0800

    modify test futureTask

--- src/main/java/com/zhyyu/learn/concurrency/util/FutureTaskTest.java
+++ src/main/java/com/zhyyu/learn/concurrency/util/FutureTaskTest.java
@@ -12,14 +12,26 @@ import java.util.concurrent.FutureTask;
 public class FutureTaskTest {
 
 	public static void main(String[] args) throws InterruptedException, ExecutionException {
-		FutureTask<Integer> futureTask = new FutureTask<>(new MyCallable());
+		/*FutureTask<Integer> futureTask = new FutureTask<>(new MyCallable());
 		new Thread(futureTask).start();
 		System.out.println("start future task");
 		
 		System.out.println("getting future result");
 		Integer integer = futureTask.get();
 		
-		System.out.println("result: " + integer);
+		System.out.println("result: " + integer);*/
+		
+		
+		// other way to run future task
+		/**
+		 * run 方法是阻塞方法, task 执行完后放行, 后可以get 到task 执行结果
+		 */
+		FutureTask<Integer> futureTask2 = new FutureTask<>(new MyCallable());
+		futureTask2.run();
+		System.out.println("futureTask2 is running");
+		
+		Integer integer2 = futureTask2.get();
+		System.out.println("futureTask2 get result: " + integer2);
 	}
 	
 	public static class MyCallable implements Callable<Integer> {
