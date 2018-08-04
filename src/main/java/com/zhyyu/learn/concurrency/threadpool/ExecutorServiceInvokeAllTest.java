a5c236f249a011cec55dd59955f5665be620410f
= <zhyyu2016@gmail.com>
2018-08-04 17:35:08 +0800

    测试executorService.invokeAll, 返回futureList 和 callableList 顺序一致

---
+++ src/main/java/com/zhyyu/learn/concurrency/threadpool/ExecutorServiceInvokeAllTest.java
@@ -0,0 +1,55 @@
+package com.zhyyu.learn.concurrency.threadpool;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import org.apache.commons.lang3.RandomUtils;
+
+/**
+ * 测试executorService.invokeAll, 返回futureList 和 callableList 顺序一致
+ * <br>
+ * Returns:a list of Futures representing the tasks, in the same sequential order as produced by the iterator for the given task list, each of which has completed
+ * @author zhyyu
+ *
+ */
+public class ExecutorServiceInvokeAllTest {
+
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+		ExecutorService executorService = Executors.newSingleThreadExecutor();
+		
+		List<Callable<Integer>> callableList = new ArrayList<>();
+		for (int i = 0; i < 10; i++) {
+			callableList.add(new MyCallable(i));
+		}
+		
+		System.out.println("try invoke all task");
+		List<Future<Integer>> futureList = executorService.invokeAll(callableList);
+		
+		System.out.println("task result:");
+		for (Future<Integer> future : futureList) {
+			System.out.println(future.get());
+		}
+	}
+	
+	public static class MyCallable implements Callable<Integer> {
+
+		private int i;
+		
+		public MyCallable(int i) {
+			this.i = i;
+		}
+
+		@Override
+		public Integer call() throws Exception {
+			Thread.sleep(RandomUtils.nextInt(0, 100));
+			return i;
+		}
+		
+	}
+	
+}
