51244561edcf3b1ee531a6d8046a9064fb8266d0
zhongyu_yu <zhongyu_yu@kingdee.com>
2018-08-09 10:45:52 +0800

    test worker thread num greater than threadpool size

--- src/main/java/com/zhyyu/learn/concurrency/threadpool/ExecutorServiceInvokeAllTest.java
+++ src/main/java/com/zhyyu/learn/concurrency/threadpool/ExecutorServiceInvokeAllTest.java
@@ -8,19 +8,23 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-import org.apache.commons.lang3.RandomUtils;
-
 /**
  * 测试executorService.invokeAll, 返回futureList 和 callableList 顺序一致, invokeAll 为阻塞方法, 等到所有任务完成
  * <br>
  * Returns:a list of Futures representing the tasks, in the same sequential order as produced by the iterator for the given task list, each of which has completed
+ * 
+ * OUTPUT:
+ * invoke all spent mills: 1302
+ * CON:
+ * 花费时间不为最大线程执行时间900ms, 因为线程池大小为5, 工作线程有10 个
+ * 
  * @author zhyyu
  *
  */
 public class ExecutorServiceInvokeAllTest {
 
 	public static void main(String[] args) throws InterruptedException, ExecutionException {
-		ExecutorService executorService = Executors.newSingleThreadExecutor();
+		ExecutorService executorService = Executors.newFixedThreadPool(5);
 		
 		List<Callable<Integer>> callableList = new ArrayList<>();
 		for (int i = 0; i < 10; i++) {
