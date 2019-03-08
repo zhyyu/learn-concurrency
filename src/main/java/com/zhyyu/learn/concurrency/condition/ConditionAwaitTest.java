d7aebeea1c2549bcc93547e4846b11a0808dbd17
zhyyu <zhyyu2016@gmail.com>
2019-03-09 00:05:35 +0800

    ConditionAwaitTest add jstack

--- src/main/java/com/zhyyu/learn/concurrency/condition/ConditionAwaitTest.java
+++ src/main/java/com/zhyyu/learn/concurrency/condition/ConditionAwaitTest.java
@@ -13,13 +13,13 @@ import java.util.concurrent.locks.ReentrantLock;
  * CON:
  * 与object.wait() 条件队列基本相同, 不同点:
  * 1. object.wait 是使用this 作为锁, 生成唯一条件队列. 想当于"object.exclusiveCondition.await object.exclusiveCondition.signal" 
- * 2. Reentrantant 是自定义锁, 通过newCondion 生成自定义条件队列, 没一个条件队列仅接收该条件队列的signal 消息
+ * 2. Reentrantant 是自定义锁, 通过newCondion 生成自定义条件队列, 每一个条件队列仅接收该条件队列的signal 消息
  * 
  * 相同点:
  * 1. 均需要条件谓词
- * 2. 提交谓词必须由lock 保护
+ * 2. 条件谓词必须由lock 保护
  * 3. while(true)
- * 4. await, signal, signal 均需在lock 块
+ * 4. await, signal, signalAll 均需在lock 块
  * 
  * @author zhyyu2016
  *
@@ -49,3 +49,13 @@ public class ConditionAwaitTest {
 	}
 	
 }
+
+
+//"main" #1 prio=5 os_prio=0 tid=0x0000000003503000 nid=0x262c waiting on condition [0x000000000318e000]
+//		   java.lang.Thread.State: WAITING (parking)
+//		        at sun.misc.Unsafe.park(Native Method)
+//		        - parking to wait for  <0x00000000d6252bc8> (a java.util.concurrent.locks.AbstractQueuedSynchronizer$ConditionObject)
+//		        at java.util.concurrent.locks.LockSupport.park(LockSupport.java:175)
+//		        at java.util.concurrent.locks.AbstractQueuedSynchronizer$ConditionObject.await(AbstractQueuedSynchronizer.java:2039)
+//		        at com.zhyyu.learn.concurrency.condition.ConditionAwaitTest$MyObject.condition1AwaitWithLock(ConditionAwaitTest.java:46)
+//		        at com.zhyyu.learn.concurrency.condition.ConditionAwaitTest.main(ConditionAwaitTest.java:32)
