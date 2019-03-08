b988ca374469893ab99380594e4a5c57326ff3af
zhyyu <zhyyu2016@gmail.com>
2019-03-09 00:57:04 +0800

    CountDownLatchTest add jstack

--- src/main/java/com/zhyyu/learn/concurrency/util/CountDownLatchTest.java
+++ src/main/java/com/zhyyu/learn/concurrency/util/CountDownLatchTest.java
@@ -59,3 +59,28 @@ public class CountDownLatchTest {
 	}
 	
 }
+
+
+//"Thread-9" #20 prio=5 os_prio=0 tid=0x0000000019739000 nid=0x2074 waiting on condition [0x000000001ad9f000]
+//		   java.lang.Thread.State: WAITING (parking)
+//		        at sun.misc.Unsafe.park(Native Method)
+//		        - parking to wait for  <0x00000000d6250568> (a java.util.concurrent.CountDownLatch$Sync)
+//		        at java.util.concurrent.locks.LockSupport.park(LockSupport.java:175)
+//		        at java.util.concurrent.locks.AbstractQueuedSynchronizer.parkAndCheckInterrupt(AbstractQueuedSynchronizer.java:836)
+//		        at java.util.concurrent.locks.AbstractQueuedSynchronizer.doAcquireSharedInterruptibly(AbstractQueuedSynchronizer.java:997)
+//		        at java.util.concurrent.locks.AbstractQueuedSynchronizer.acquireSharedInterruptibly(AbstractQueuedSynchronizer.java:1304)
+//		        at java.util.concurrent.CountDownLatch.await(CountDownLatch.java:231)
+//		        at com.zhyyu.learn.concurrency.util.CountDownLatchTest$MyTask.run(CountDownLatchTest.java:47)
+//		        at java.lang.Thread.run(Thread.java:748)
+//
+//"Thread-8" #19 prio=5 os_prio=0 tid=0x0000000019738800 nid=0x3b2c waiting on condition [0x000000001ac9f000]
+//   java.lang.Thread.State: WAITING (parking)
+//        at sun.misc.Unsafe.park(Native Method)
+//        - parking to wait for  <0x00000000d6250568> (a java.util.concurrent.CountDownLatch$Sync)
+//        at java.util.concurrent.locks.LockSupport.park(LockSupport.java:175)
+//        at java.util.concurrent.locks.AbstractQueuedSynchronizer.parkAndCheckInterrupt(AbstractQueuedSynchronizer.java:836)
+//        at java.util.concurrent.locks.AbstractQueuedSynchronizer.doAcquireSharedInterruptibly(AbstractQueuedSynchronizer.java:997)
+//        at java.util.concurrent.locks.AbstractQueuedSynchronizer.acquireSharedInterruptibly(AbstractQueuedSynchronizer.java:1304)
+//        at java.util.concurrent.CountDownLatch.await(CountDownLatch.java:231)
+//        at com.zhyyu.learn.concurrency.util.CountDownLatchTest$MyTask.run(CountDownLatchTest.java:47)
+//        at java.lang.Thread.run(Thread.java:748)
